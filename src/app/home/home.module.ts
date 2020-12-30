978996a55795799dbd58f4a0d0ad87bf43d6b250
jairescamilla <hernandez_yo14@outlook.es>
2020-12-30 14:40:43 -0600

    Generating search field

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SearchComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
