540bc3bd2ed11d5b7b043661eff0026d806ec505
jairescamilla <hernandez_yo14@outlook.es>
2021-01-04 17:37:38 -0600

    Sending updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'github-jobs';
 
+  constructor(
+    private swUpdate: SwUpdate
+  ){
+  }
 
+  ngOnInit(){
+    this.updatePWA();
+  }
+
+  updatePWA(){
+    this.swUpdate.available.subscribe(value => {
+      console.log("update");
+      console.log(value);
+      window.location.reload();
+    })
+  }
 
 }
